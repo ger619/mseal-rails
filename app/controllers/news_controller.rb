93a5d5510b61fcb4f4fd328a2d1d98ce0f564603
ger619 <draker74@gmail.com>
2023-06-20 19:07:09 +0300

    Added News

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,40 @@
 class NewsController < ApplicationController
+
+  def index
+    @news = News.all.order('created_at DESC')
+  end
+
+  def show
+    @news = News.find(params[:id])
+  end
+
+  def new
+    @news = News.new
+  end
+
+  def create
+    @news = News.new(news_params)
+
+    respond_to do |format|
+      if @news.save
+        format.html { redirect_to news_index_path(@news), notice: 'News was successfully created.' }
+      else
+        format.html { redirect_to news_index_url, notice: 'Failure' }
+      end
+    end
+  end
+
+  def destroy
+    @news = News.find(params[:id])
+    @news.delete
+    respond_to do |format|
+      format.html { redirect_to news_index_path, notice: 'News was successfully deleted.' }
+    end
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:type_of_news, :header_news, :body, :image)
+  end
 end
