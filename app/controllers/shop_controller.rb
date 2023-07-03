e045b8a36a79929a5c090c9e1e577f9891fe2d49
ger619 <draker74@gmail.com>
2023-07-03 13:56:10 +0300

    add news show

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,4 +2,41 @@ class ShopController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index; end
+
+
+  def show;
+    @shop = Shop.find(params[:id])
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    @shop.user = current_user
+    respond_to do |format|
+      if @shop.save
+        format.html { redirect_to shop_index_path(@shop), notice: 'Shop was successfully created.' }
+      else
+        format.html { redirect_to shop_index_url, notice: 'Failure' }
+      end
+    end
+  end
+
+  def destroy
+    @shop = Shop.find(params[:id])
+    @shop.user = current_user
+    @shop.delete
+    respond_to do |format|
+      format.html { redirect_to shop_index_path, notice: 'Shop was successfully deleted.' }
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:product_name, :description, :size, :quantity, :price, :photo)
+  end
+
 end
