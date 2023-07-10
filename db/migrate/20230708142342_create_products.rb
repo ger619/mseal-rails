b7d4317ac4b011ebf67c3c3fbe21bcb3cbb97af5
ger619 <draker74@gmail.com>
2023-07-10 19:19:05 +0300

    Add quantity and size when buying

--- db/migrate/20230708142342_create_products.rb
+++ db/migrate/20230708142342_create_products.rb
@@ -3,7 +3,6 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.text :name
       t.string :description
-      t.string :size
       t.integer :price
       t.string :photo_product
 
