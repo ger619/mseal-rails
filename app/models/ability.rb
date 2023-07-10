15b808634eba68dfcff6edac39405ebff497c4a0
ger619 <draker74@gmail.com>
2023-07-10 13:54:29 +0300

    Add devise roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,4 @@
 class Ability
   include CanCan::Ability
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Include default devise modules. Others available are:
+
+  enum role: [:user, :moderator, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+    def set_default_role
+      self.role ||= :user
+    end
+
   has_many :news, foreign_key: :user_id, class_name: 'News', dependent: :destroy
   has_many :clubs, foreign_key: :user_id, class_name: 'Club', dependent: :destroy
 end
