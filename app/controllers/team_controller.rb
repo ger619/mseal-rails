e93b3f92248831946f47c2b998db3f3ad78ac0cb
ger619 <draker74@gmail.com>
2023-07-22 01:04:56 +0300

    Add teams clean up

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,5 +1,4 @@
 class TeamController < ApplicationController
-  skip_before_action :authenticate_user!, except: [:index]
   before_action :authenticate_user!, except: %i[index show]
 
   def index; end
