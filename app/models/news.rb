76df8a238bb18aa33fb0b123534025b57aa2309f
ger619 <draker74@gmail.com>
2024-03-20 14:10:11 +0300

    add news validation on the models

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,10 +1,9 @@
 class News < ApplicationRecord
   belongs_to :user, class_name: 'User', optional: true
   has_one_attached :image
-  #Validations on a model
+  # Validations on a model
   validates :type_of_news, presence: true
   validates :header_news, presence: true
   validates :body, presence: true
   validates :image, presence: true
-
 end
