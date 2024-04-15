01048419d5d261a9177622a75b63478ebf75f888
ger619 <draker74@gmail.com>
2024-04-15 21:32:06 +0300

    add all authorization using cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,6 @@ class Ability
       can :manage, Motor::Admin, all
     elsif user.role == 'moderator'
       can :manage, [Advert, Club, HallOfFame, News, Opponent, OpponentTeam, Product, Team], :all
-
     else
       user.role
       can :read, :all
