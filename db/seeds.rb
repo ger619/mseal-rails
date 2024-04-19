bf00e504cf2bce177f629ea098e6b285cc0f40d7
ger619 <draker74@gmail.com>
2024-04-19 19:24:09 +0300

    added devise trackable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Seed to add o to all users role field that is null
+
