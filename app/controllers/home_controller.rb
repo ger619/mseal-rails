206abefd490c2da9a8c6a0cb9faa3c4d170efd3b
ger619 <draker74@gmail.com>
2024-04-19 14:06:07 +0300

    adding next match details without results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   def index
     @news = News.all.order('created_at DESC')
     @advert = Advert.all.order('created_at DESC')
-    @opponent = Opponent.all.order('created_at DESC')
+    # Display all opponent in ascending order if date is greater than today
+    @opponent = Opponent.limit(1).order('match_date ASC').where('match_date >= ?', Date.today)
   end
 end
