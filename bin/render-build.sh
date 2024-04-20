3e86591fff2870bf95c984490302781f7ed4ea49
ger619 <draker74@gmail.com>
2024-04-20 07:35:12 +0300

    Add all teams

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:reset
-bundle exec rails db:migrate
+bundle exec rails db:create
+bundle exec rails db:migrate RAILS_ENV=production
