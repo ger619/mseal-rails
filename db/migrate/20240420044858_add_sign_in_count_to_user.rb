d97410c051b61c0c7261004ff001afc0252f029f
ger619 <draker74@gmail.com>
2024-04-20 13:42:06 +0300

    add more content

--- db/migrate/20240420044858_add_sign_in_count_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddSignInCountToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :sign_in_count, :integer, default: 0, null: false
-  end
-end
---
+++ db/schema.rb
@@ -0,0 +1,410 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2024_04_19_145510) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.uuid "record_id", null: false
+    t.uuid "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "adverts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "type_of_advert"
+    t.string "header_advert"
+    t.string "mobile_image"
+    t.string "desk_image"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_adverts_on_user_id"
+  end
+
+  create_table "carts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clubs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "header"
+    t.string "body"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "user_id", null: false
+    t.index ["user_id"], name: "index_clubs_on_user_id"
+  end
+
+  create_table "hall_of_fames", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "goals"
+    t.string "saves"
+    t.string "blocks"
+    t.index ["team_id"], name: "index_hall_of_fames_on_team_id"
+  end
+
+  create_table "memberships", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "type_of_membership"
+    t.string "title"
+    t.string "country"
+    t.date "date_of_birth"
+    t.string "email"
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
+
+  create_table "motor_alert_locks", force: :cascade do |t|
+    t.bigint "alert_id", null: false
+    t.string "lock_timestamp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["alert_id", "lock_timestamp"], name: "index_motor_alert_locks_on_alert_id_and_lock_timestamp", unique: true
+    t.index ["alert_id"], name: "index_motor_alert_locks_on_alert_id"
+  end
+
+  create_table "motor_alerts", force: :cascade do |t|
+    t.bigint "query_id", null: false
+    t.string "name", null: false
+    t.text "description"
+    t.text "to_emails", null: false
+    t.boolean "is_enabled", default: true, null: false
+    t.text "preferences", null: false
+    t.bigint "author_id"
+    t.string "author_type"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "motor_alerts_name_unique_index", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["query_id"], name: "index_motor_alerts_on_query_id"
+    t.index ["updated_at"], name: "index_motor_alerts_on_updated_at"
+  end
+
+  create_table "motor_api_configs", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "url", null: false
+    t.text "preferences", null: false
+    t.text "credentials", null: false
+    t.text "description"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "motor_api_configs_name_unique_index", unique: true, where: "(deleted_at IS NULL)"
+  end
+
+  create_table "motor_audits", force: :cascade do |t|
+    t.string "auditable_id"
+    t.string "auditable_type"
+    t.string "associated_id"
+    t.string "associated_type"
+    t.bigint "user_id"
+    t.string "user_type"
+    t.string "username"
+    t.string "action"
+    t.text "audited_changes"
+    t.bigint "version", default: 0
+    t.text "comment"
+    t.string "remote_address"
+    t.string "request_uuid"
+    t.datetime "created_at"
+    t.index ["associated_type", "associated_id"], name: "motor_auditable_associated_index"
+    t.index ["auditable_type", "auditable_id", "version"], name: "motor_auditable_index"
+    t.index ["created_at"], name: "index_motor_audits_on_created_at"
+    t.index ["request_uuid"], name: "index_motor_audits_on_request_uuid"
+    t.index ["user_id", "user_type"], name: "motor_auditable_user_index"
+  end
+
+  create_table "motor_configs", force: :cascade do |t|
+    t.string "key", null: false
+    t.text "value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_motor_configs_on_key", unique: true
+    t.index ["updated_at"], name: "index_motor_configs_on_updated_at"
+  end
+
+  create_table "motor_dashboards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.text "preferences", null: false
+    t.bigint "author_id"
+    t.string "author_type"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "motor_dashboards_title_unique_index", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["updated_at"], name: "index_motor_dashboards_on_updated_at"
+  end
+
+  create_table "motor_forms", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.text "api_path", null: false
+    t.string "http_method", null: false
+    t.text "preferences", null: false
+    t.bigint "author_id"
+    t.string "author_type"
+    t.datetime "deleted_at"
+    t.string "api_config_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "motor_forms_name_unique_index", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["updated_at"], name: "index_motor_forms_on_updated_at"
+  end
+
+  create_table "motor_note_tag_tags", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "note_id", null: false
+    t.index ["note_id", "tag_id"], name: "motor_note_tags_note_id_tag_id_index", unique: true
+    t.index ["tag_id"], name: "index_motor_note_tag_tags_on_tag_id"
+  end
+
+  create_table "motor_note_tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "motor_note_tags_name_unique_index", unique: true
+  end
+
+  create_table "motor_notes", force: :cascade do |t|
+    t.text "body"
+    t.bigint "author_id"
+    t.string "author_type"
+    t.string "record_id", null: false
+    t.string "record_type", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id", "author_type"], name: "motor_notes_author_id_author_type_index"
+    t.index ["record_id", "record_type"], name: "motor_notes_record_id_record_type_index"
+  end
+
+  create_table "motor_notifications", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.bigint "recipient_id", null: false
+    t.string "recipient_type", null: false
+    t.string "record_id"
+    t.string "record_type"
+    t.string "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id", "recipient_type"], name: "motor_notifications_recipient_id_recipient_type_index"
+    t.index ["record_id", "record_type"], name: "motor_notifications_record_id_record_type_index"
+  end
+
+  create_table "motor_queries", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.text "sql_body", null: false
+    t.text "preferences", null: false
+    t.bigint "author_id"
+    t.string "author_type"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "motor_queries_name_unique_index", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["updated_at"], name: "index_motor_queries_on_updated_at"
+  end
+
+  create_table "motor_reminders", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "author_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "recipient_type", null: false
+    t.string "record_id"
+    t.string "record_type"
+    t.datetime "scheduled_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id", "author_type"], name: "motor_reminders_author_id_author_type_index"
+    t.index ["recipient_id", "recipient_type"], name: "motor_reminders_recipient_id_recipient_type_index"
+    t.index ["record_id", "record_type"], name: "motor_reminders_record_id_record_type_index"
+    t.index ["scheduled_at"], name: "index_motor_reminders_on_scheduled_at"
+  end
+
+  create_table "motor_resources", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "preferences", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_motor_resources_on_name", unique: true
+    t.index ["updated_at"], name: "index_motor_resources_on_updated_at"
+  end
+
+  create_table "motor_taggable_tags", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "taggable_id", null: false
+    t.string "taggable_type", null: false
+    t.index ["tag_id"], name: "index_motor_taggable_tags_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "tag_id"], name: "motor_polymorphic_association_tag_index", unique: true
+  end
+
+  create_table "motor_tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "motor_tags_name_unique_index", unique: true
+  end
+
+  create_table "news", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "type_of_news"
+    t.string "header_news"
+    t.string "body"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "user_id", null: false
+    t.index ["user_id"], name: "index_news_on_user_id"
+  end
+
+  create_table "opponent_teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "team_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "user_id"
+    t.index ["user_id"], name: "index_opponent_teams_on_user_id"
+  end
+
+  create_table "opponents", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.date "match_date"
+    t.time "match_time"
+    t.string "venue"
+    t.string "tournament"
+    t.integer "score_one"
+    t.integer "score_two"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "opponent_team_id"
+    t.index ["opponent_team_id"], name: "index_opponents_on_opponent_team_id"
+    t.index ["user_id"], name: "index_opponents_on_user_id"
+  end
+
+  create_table "orderables", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "product_id", null: false
+    t.uuid "cart_id", null: false
+    t.string "size"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orderables_on_cart_id"
+    t.index ["product_id"], name: "index_orderables_on_product_id"
+  end
+
+  create_table "products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "name"
+    t.string "description"
+    t.integer "price"
+    t.string "photo_product"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "shops", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "product_name"
+    t.string "description"
+    t.string "size"
+    t.string "quantity"
+    t.string "price"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "first_name"
+    t.string "second_name"
+    t.string "last_name"
+    t.string "position"
+    t.string "image"
+    t.integer "jersey_number"
+    t.string "about"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teams_on_user_id"
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "adverts", "users"
+  add_foreign_key "clubs", "users"
+  add_foreign_key "hall_of_fames", "teams"
+  add_foreign_key "motor_alert_locks", "motor_alerts", column: "alert_id"
+  add_foreign_key "motor_alerts", "motor_queries", column: "query_id"
+  add_foreign_key "motor_note_tag_tags", "motor_note_tags", column: "tag_id"
+  add_foreign_key "motor_note_tag_tags", "motor_notes", column: "note_id"
+  add_foreign_key "motor_taggable_tags", "motor_tags", column: "tag_id"
+  add_foreign_key "news", "users"
+  add_foreign_key "opponent_teams", "users"
+  add_foreign_key "opponents", "opponent_teams"
+  add_foreign_key "opponents", "users"
+  add_foreign_key "orderables", "carts"
+  add_foreign_key "orderables", "products"
+  add_foreign_key "products", "users"
+  add_foreign_key "teams", "users"
+end
