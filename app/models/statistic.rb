ca72e19713d7b6f7104c854306b1eb797a1c7301
ger619 <draker74@gmail.com>
2024-04-24 19:07:55 +0300

    adding validation and uniqueness

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -29,4 +29,8 @@
 class Statistic < ApplicationRecord
   belongs_to :team
   belongs_to :user
+
+  validates :fkfcup_appearance, :fkfcup_goal, :fkfcup_redcard, :fkfcup_yellow, :kpl_appearance, :kpl_goal,
+            :kpl_redcard, :kpl_yellow, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  validates :team_id, uniqueness: true
 end
