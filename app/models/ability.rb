55851475e039b4e08b6e10fd1bfade9ff6d5bae4
ger619 <draker74@gmail.com>
2024-05-18 11:56:54 +0300

    added changes to the news

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,14 @@ class Ability
     can :read, [Advert, Club, HallOfFame, News, Opponent, OpponentTeam, Product, Team, Statistic, Season, Scorer]
     cannot :manage, Motor::Admin
 
-    return if user.admin?
+    return unless user.role == 'moderator'
+
+    can :manage, [Advert, Club, HallOfFame, News, Opponent, OpponentTeam, Product, Team, Statistic, Season, Scorer]
+
+    return unless user.role == 'admin'
 
     can :manage,
-        [Advert, Club, HallOfFame, News, Opponent, OpponentTeam, Product, Team, Statistic, Season, Scorer,
-         { user_id: user.id }]
+        Advert, Club, HallOfFame, News, Opponent, OpponentTeam, Product, Team, Statistic, Season, Scorer, Motor::Admin,
+        user.role == 'admin'
   end
 end
