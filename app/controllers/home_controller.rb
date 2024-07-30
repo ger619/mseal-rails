30ef497e5a53f4f01aa72a9daa1e90654a85117d
ger619 <draker74@gmail.com>
2024-07-30 15:13:32 +0300

    add all N+1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @news = News.all.order('created_at DESC')
+    @news = News.all.includes(icon_attachment: :blob).order('created_at DESC')
     @advert = Advert.all.order('created_at DESC')
     # Display all opponent in ascending order if date is greater than today
     @opponent1 = Opponent.where('match_date >=?', Date.today).order('match_date ASC')
