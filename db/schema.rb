e06fc9fe568d7a0b493a80692208a1a1c1d853ac
Robert Kanyoro <rmjesusaddict@gmail.com>
2024-12-14 18:31:02 +0300

    Adjust the design of the articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_24_082001) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_14_133919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -454,7 +454,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_24_082001) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
@@ -466,6 +465,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_24_082001) do
     t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sign_in_count"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
