bbbcaca423b371bbe67d2070248651ca3d34e9f9
ger619 <abolger254@gmail.com>
2024-12-18 19:35:35 +0300

    Clean up on fixtures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     # Display all opponent in ascending order if date is greater than today
     @upcoming = Opponent.where('match_date >=?', Date.today).order('match_date ASC')
     @results = Opponent.where('match_date <= ?',
-                                Date.today).where.not(score_one: nil).where.not(score_two: nil).order('match_date DESC')
+                              Date.today).where.not(score_one: nil).where.not(score_two: nil).order('match_date DESC')
     @product = Product.includes(%i[photo_product_attachment rich_text_content]).order('created_at DESC')
 
     @user = User.all
