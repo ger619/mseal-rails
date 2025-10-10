838131403630490f0dfea8097fe11052d4b67780
@ger619 <abolger254@gmail.com>
2025-10-10 12:25:14 +0300

    Add XML migration files and improve nil handling in opponent_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
